61071b7a1011ff685abdecf8045285023be32ab1
Devinhollister-graham <devin-hg@mihlfeld.com>
2020-04-30 12:27:50 -0500

    Fix app controller for 4.0

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -4,7 +4,7 @@ namespace CakeSwagger\Controller;
 
 use App\Controller\AppController as BaseController;
 use Cake\Core\Configure;
-use Cake\Event\Event;
+use Cake\Event\EventInterface;
 use Cake\Filesystem\File;
 use Cake\Utility\Hash;
 use CakeSwagger\Exception\CakeSwaggerException;
@@ -12,70 +12,62 @@ use CakeSwagger\Exception\CakeSwaggerException;
 class AppController extends BaseController
 {
 
-	/**
-	 * @var array that will hold merged configuration settings.
-	 */
-	protected $config = [];
+  /**
+   * @var array that will hold merged configuration settings.
+   */
+  protected $config = [];
 
-	/**
-	 * @var array that will hold merged options settings.
-	 */
-	protected $options = [];
+  /**
+   * @var array that will hold merged options settings.
+   */
+  protected $options = [];
 
-	/**
-	 * @var array holding required default configuration settings.
-	 */
-	public static $defaultConfig = [
-		'ui' => [
-			'title' => 'CakePHP Swagger plugin'
-		],
-		'route' => [
-			'path' => '/api'
-		]
-	];
+  /**
+   * @var array holding required default configuration settings.
+   */
+  public static $defaultConfig = [
+    'ui' => [
+      'title' => 'CakePHP Swagger plugin'
+    ],
+    'route' => [
+      'path' => '/api'
+    ]
+  ];
 
-	/**
-	 * Initialize
-	 */
-	public function initialize(): void
-	{
-		parent::initialize();
+  /**
+   * Initialize
+   */
+  public function initialize(): void
+  {
+    parent::initialize();
 
-		if (Configure::check('CakeSwagger')) {
-			$this->config = Hash::merge(static::$defaultConfig, Configure::read('CakeSwagger'));
-		}
-		if (array_key_exists('analyser', $this->config)) {
-			$this->options['analyser'] = $this->config['analyser'];
-		}
-		if (array_key_exists('analysis', $this->config)) {
-			$this->options['analysis'] = $this->config['analysis'];
-		}
-		if (array_key_exists('processors', $this->config)) {
-			$this->options['processors'] = $this->config['processors'];
-		}
-		if (array_key_exists('exclude', $this->config)) {
-			$this->options['exclude'] = $this->config['exclude'];
-		}
-	}
+    if (Configure::check('CakeSwagger')) {
+      $this->config = Hash::merge(static::$defaultConfig, Configure::read('CakeSwagger'));
+    }
+    if (array_key_exists('analyser', $this->config)) {
+      $this->options['analyser'] = $this->config['analyser'];
+    }
+    if (array_key_exists('analysis', $this->config)) {
+      $this->options['analysis'] = $this->config['analysis'];
+    }
+    if (array_key_exists('processors', $this->config)) {
+      $this->options['processors'] = $this->config['processors'];
+    }
+    if (array_key_exists('exclude', $this->config)) {
+      $this->options['exclude'] = $this->config['exclude'];
+    }
+  }
 
-	/**
-	 * @param Event $event
-	 * @return \Cake\Http\Response|null|void
-	 * @throws \CakeSwagger\Exception\CakeSwaggerException
-	 */
-	public function beforeFilter(Event $event): void
-	{
-		parent::beforeFilter($event);
+  /**
+   * @param Event $event
+   * @return \Cake\Http\Response|null|void
+   * @throws \CakeSwagger\Exception\CakeSwaggerException
+   */
+  public function beforeFilter(EventInterface $event)
+  {
+    parent::beforeFilter($event);
 
-		$this->viewBuilder()->setLayout('CakeSwagger.default');
-	}
-
-	/**
-	 * @param Event $event
-	 */
-	public function beforeRender(Event $event): void
-	{
-		parent::beforeRender($event);
-	}
+    $this->viewBuilder()->setLayout('CakeSwagger.default');
+  }
 
 }
